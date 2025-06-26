6abe07d2ebb4a8a210a6dee1dfa5f47e52fd2a4c
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-06-25 21:46:12 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,60 @@
+export const eventData = {
+    couple: {
+      names: "Mi Bautizo",
+      date: "27 . 06 . 2026",
+      heroImage: "/images/angelito.svg"
+    },
+    song: {
+      title: "Aleluya",
+      file: "/Aleluya (Violín).mp3"
+    },
+    ceremony: {
+      ceremonyImage:"/images/ceremon.png",
+      place: "Escuela de Cristo",
+      address: "Calle de los Pasos, Antigua Guatemala",
+      date: "27 de Junio",
+      time: "10:00 hrs",
+      mapLink: "https://maps.app.goo.gl/UVVW3m3RmkScoYxP6"
+    },
+    reception: {
+      receptionImage:"/images/reception.png",
+      place: "Convento Las Capuchinas",
+      address: "2a Avenida Norte y 2a Calle Oriente, Antigua Guatemala",
+      date: "14 de Junio",
+      time: "13:00 hrs",
+      mapLink: "https://maps.app.goo.gl/43FEb8KmmDKWhGgh9"
+    },
+    dresscode: {
+      description: "Traje formal, los colores Blanco, Ivory, Amarillo y Beige quedan reservados.",
+      image: "/images/dresscode.png",
+      inspiration: {
+        women: "https://www.pinterest.com/search/pins/?q=Vestidos%20invitadas%20de%20boda%20de%20día",
+        men: "https://www.pinterest.com/search/pins/?q=Traje%20formal%20de%20hombres%20invitados%20a%20boda"
+      }
+    },  
+    gallery: [
+      "/images/V1.jpg",
+      "/images/V2.jpg",
+      "/images/V3.jpg",
+      "/images/V4.jpg",
+      "/images/V5.jpg",
+      "/images/V6.jpg",
+    ],
+
+    finalPhoto: "/images/10.png",
+    finalMessage: "¡Gracias por ser parte de este día tan especial!",
+    rsvp: {
+      rsvpImage: "/images/rsvp.png",
+      whatsapp: "https://wa.me/50236011737?text=Confirmo%20mi%20asistencia%20a%20la%20boda",
+      email: "mailto:hello@thetwodesign.com"
+    },
+    footer: {
+      social: {
+        whatsapp: "https://wa.me/50236011737",
+        facebook: "https://www.facebook.com/thetwodesign",
+        instagram: "https://www.instagram.com/thetwodesign"
+      },
+      logo: "/images/logo.png",
+    }
+  };
+  
